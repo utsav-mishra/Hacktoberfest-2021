8d8d39e81b9ef3e5dfd0f4cce9e332bf86004aff
Aakash Jain <aakashjain1192000@gmail.com>
2021-10-04 15:59:54 +0530

    reverse_A_LinkedList.java

---
+++ Leetcode-Solutions/reverse_A_LinkedList.java
@@ -0,0 +1,17 @@
+ 
+// leetcode 206 - Reverse Linked List
+
+    public static ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode forw = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
